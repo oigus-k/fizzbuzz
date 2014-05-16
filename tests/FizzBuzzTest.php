44053ab46c95a1c8b3a8cd0ffad76fba269aaf4d
Keita SUGIO <k_sugio@yumemi.co.jp>
2014-05-16 17:36:29 +0900

    テストケース追加

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -7,20 +7,35 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase
     const FIZZ = "Fizz";
     const BUZZ = "Buzz";
 
-    public function testNumIs3(){
-        $num = 3;
+    public function testNumIsFizzValue(){
+        $num = [3, 6, 9, 12, 18, 21, 24, 27, 33, 36];
         $fizzbuzz = new FizzBuzz();
-        $this->assertEquals(self::FIZZ, $fizzbuzz->judgeFizzBuzz($num));
+        for($i=0; $i<count($num); $i++){
+            $this->assertEquals(self::FIZZ, $fizzbuzz->judgeFizzBuzz($num[$i]));
+        }
     }
 
-    public function testNumIs5(){
-        $num = 5;
+    public function testNumIsBuzzValue(){
+        $num = [5, 10, 20, 25, 35, 40, 50, 55, 65, 70];
         $fizzbuzz = new FizzBuzz();
-        $this->assertEquals(self::BUZZ, $fizzbuzz->judgeFizzBuzz($num));
+        for($i=0; $i<count($num); $i++){
+            $this->assertEquals(self::BUZZ, $fizzbuzz->judgeFizzBuzz($num[$i]));
+        }
     }
-    public function testNumIs15(){
-        $num = 15;
+    
+    public function testNumIsFizzBuzzValue(){
+        $num = [15, 30, 45, 60, 75, 90, 105, 120, 135, 150];
         $fizzbuzz = new FizzBuzz();
-        $this->assertEquals(self::FIZZ . self::BUZZ, $fizzbuzz->judgeFizzBuzz($num));
+        for($i=0; $i<count($num); $i++){
+            $this->assertEquals(self::FIZZ . self::BUZZ, $fizzbuzz->judgeFizzBuzz($num[$i]));
+        }
+    }
+
+    public function testNumIsNaturalValue(){
+        $num = [1, 2, 4, 7, 8, 11, 13, 14, 16, 17];
+        $fizzbuzz = new FizzBuzz();
+        for($i=0; $i<count($num); $i++){
+            $this->assertEquals($num[$i], $fizzbuzz->judgeFizzBuzz($num[$i]));
+        }
     }
 }
